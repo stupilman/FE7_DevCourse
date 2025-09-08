d3c566d4141f47f186d2f4df938a1b8be911895c
stupilman <creworroad@naver.com>
2025-09-08 16:38:48 +0900

    20250908 수정

---
+++ learn-ts/02/src/ex02.ts
@@ -0,0 +1,29 @@
+{
+  // 1.
+  // 함수 정의
+  const sumAllNumbers = (...nums: number[][]) => {
+    const newArr = nums.map((arr) => arr.reduce((acc, cur) => acc + cur), 0);
+    return newArr;
+  };
+
+  // 함수 호출
+  const result = sumAllNumbers([1, 2], [3, 4, 5], [6, 7]);
+  console.log(result); // [3, 12, 13]
+}
+{
+  // 2.
+  // 함수 정의
+  const doubleValue = (el: number | string) => {
+    if (typeof el === "number") return el * 2;
+    else return el.length * 2;
+  };
+
+  // 함수 호출
+  const result1 = doubleValue(5);
+  const result2 = doubleValue("hello");
+  console.log(result1); // 10
+  console.log(result2); // 10
+}
+{
+  // 3.
+}
